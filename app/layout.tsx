f2c5baaf2f4ada2bc12f18535b892e9ff9ccad25
qaq022 <parkhurstutah@gmail.com>
2026-04-30 13:46:17 +0800

    Update layout.tsx
    在layout的41行加入了百度验证，收录

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
+        <meta name="baidu-site-verification" content="codeva-scoQ51tJDO" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
