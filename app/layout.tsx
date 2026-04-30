730b63057ffca92ea9bdf89378abd4a565287ef6
qaq022 <parkhurstutah@gmail.com>
2026-04-30 14:52:09 +0800

    Change HTML lang to 'zh-CN' and add metadata
    Updated the HTML language attribute to Chinese and added metadata for SEO and Open Graph.修改和增加了网页的语言标签（英文改中文）、增加了关键词标签、增加了分享时候的标签。

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,10 +35,27 @@ export default function RootLayout({
   const serverConfig = getServerSideConfig();
 
   return (
-    <html lang="en">
+    <html lang="zh-CN">
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
+        <meta name="keywords" content="发扬滇风, AI 聊天,聊天, DeepSeek, 个人 AI 助手, yjychat" />
         <meta name="baidu-site-verification" content="codeva-scoQ51tJDO" />
+        export const metadata: Metadata = {
+  title: "发扬滇风",
+  description: "你要不要一起来发扬滇风？",
+  openGraph: {
+    title: "发扬滇风",
+    description: "你要不要一起来发扬滇风？",
+    url: "https://yjychat.uk",
+    siteName: "发扬滇风",
+    locale: "zh_CN",
+    type: "website",
+  },
+  appleWebApp: {
+    title: "发扬滇风",
+    statusBarStyle: "default",
+  },
+};
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
